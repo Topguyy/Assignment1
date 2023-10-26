8b7a19ffbfb7fd647b3bcee34b5126d67b512c6f
Mohamed El Tobgui <121908586+Topguyy@users.noreply.github.com>
2023-10-26 21:06:31 +0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity  {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView txt= findViewById(R.id.nameTxt);
+        Button btn = findViewById(R.id.signIn);
+        btn.setOnClickListener(new View.OnClickListener() {
+        @Override
+            public void onClick(View v){
+            openMainActivity2();
+
+            }
+        });
+}
+    public void openMainActivity2(){
+        Intent intent = new Intent(this,MainActivity2.class);
+        startActivity(intent);
+    }
+}
